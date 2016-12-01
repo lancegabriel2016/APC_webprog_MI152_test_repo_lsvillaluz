fa6f1c2e8c38b68d7789761d49d5d518979303e5
Lance S. Villaluz <villaluzlance3@gmail.com>
2016-12-01 10:25:25 +0800

    i change the link to index

--- exercise5/index.php
+++ exercise5/index.php
@@ -9,8 +9,9 @@ if(isset($_GET['delete_id']))
 }
 // delete condition
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>CRUD Operations With PHP and MySql - By Cleartuts</title>
@@ -31,8 +32,160 @@ function delete_id(id)
  }
 }
 </script>
+<title>My First HTML</title>
+<style>
+body { background-color : gray;
+ }
+ 
+h1 { color: white; 
+	  text-align: center;
+	  }
+	  
+p { color: white;
+}
+ul { color: yellow ;
+}
+h3 { color: yellow ;
+}
+h4 { color: yellow;
+}
+tr,table {border:2px solid black;padding:3px;}
+.sagot {font-family:Arial;font-size:14px;font-weight:bold;}
+
+.example1 {
+ height: 50px;	
+ overflow: hidden;
+ position: relative;
+}
+
+.example1 h1{
+ position: absolute;
+ width: 100%;
+ height: 100%;
+ margin: 0;
+ line-height: 50px;
+ text-align: center;
+ /* Starting position */
+ -moz-transform:translateX(100%);
+ -webkit-transform:translateX(100%);	
+ transform:translateX(100%);
+ /* Apply animation to this element */	
+ -moz-animation: example1 15s linear infinite;
+ -webkit-animation: example1 15s linear infinite;
+ animation: example1 15s linear infinite;
+}
+/* Move it (define the animation) */
+@-moz-keyframes example1 {
+ 0%   { -moz-transform: translateX(100%); }
+ 100% { -moz-transform: translateX(-100%); }
+}
+@-webkit-keyframes example1 {
+ 0%   { -webkit-transform: translateX(100%); }
+ 100% { -webkit-transform: translateX(-100%); }
+}
+@keyframes example1 {
+ 0%   { 
+ -moz-transform: translateX(100%); /* Firefox bug fix */
+ -webkit-transform: translateX(100%); /* Firefox bug fix */
+ transform: translateX(100%); 		
+ }
+ 100% { 
+ -moz-transform: translateX(-100%); /* Firefox bug fix */
+ -webkit-transform: translateX(-100%); /* Firefox bug fix */
+ transform: translateX(-100%); 
+ }
+
+
+</style>
 </head>
-<body>
+<div class="sagot">
+<script >
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Basketball.";
+	}
+	function myFunction2() {
+    document.getElementById("demo").innerHTML = "Sinigang.";
+	}
+	function myFunction3() {
+    document.getElementById("demo").innerHTML = "Facebook";
+	}
+	function myFunction4() {
+    document.getElementById("demo").innerHTML = "Defense of the Ancients 2";
+	}
+	function myFunction5() {
+    document.getElementById("demo").innerHTML = "Joemari.";
+	}
+
+</script>
+</div>
+<body background="wood.jpg">
+
+
+<div class="example1">
+<h1>Lance Gabriel Villaluz's Profile</h1>
+</div>
+
+<center>
+<img src="https://media.giphy.com/media/QQkyLVLAbQRKU/giphy-downsized-large.gif">
+</center>
+
+<h4>About Me</h4>
+<ul>
+<li>17 years old</li>
+<li>2nd year Student</li>
+<li>Asia Pacific College</li>
+<li>BSIT-MI</li>
+</ul>
+
+
+
+<center>	
+<p><h3>My Hobbies and Interests</p>
+Playing Basketball
+<p><img src="06d9ed3ba5c4630e720f6a7067004a15.jpg"></p>
+<p><a href="http://www.nba.com/">Where you can find the lastest update in Basketball</a></p>
+
+Programming
+<p><img src="Programming-Coding-Geoawesomeness.jpg" width="388" height="328"></p>
+
+
+Sleeping
+<p><img src="07c99609e7a32179fbd8544a62e86255.jpg" height="328" width="388"> 
+<br>
+Sleeping is my 
+drug,the dealer is 
+my bed and the alarm clock is the police</p>
+</center>
+
+
+<h1>Answer</h1>
+<p id="demo"></p>
+<table style="width:100%;font-size:20px;">
+<tr  rowspan="5">
+<td>
+    <h4>What is my Hobby?</h4>
+	<button type="button" onclick="myFunction()">Try it</button>
+</td>
+<td>
+    <h4>What is my favorite food?</h4>
+	<button type="button" onclick="myFunction2()">Try it</button>
+</td>
+<td>
+    <h4>What is my favorite app?</h4>
+	<button type="button" onclick="myFunction3()">Try it</button>
+</td>
+<td>
+	<h4>What is my favorite PC Game?</h4>
+	<button type="button" onclick="myFunction4()">Try it</button>
+</td>
+<td>
+	<h4>Who is my bestfriend?</h4>
+	<button type="button" onclick="myFunction5()">Try it</button>
+</td>	
+</table>
+<h3><th colspan="8"><center><a href="add_data.php">Add data here</a></th>	</h3>
+
 <center>
 
 <div id="header">
@@ -40,7 +193,7 @@ function delete_id(id)
     <label>Add Database</label>
     </div>
 </div>
-<a href = "exercise5.php"> <center><h2> Back to main page</h2></a>
+
 <div id="body">
  <div id="content">
     <table align="center">
@@ -81,5 +234,101 @@ function delete_id(id)
 </div>
 
 </center>
+
+	
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+</center>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>	
+
+
 </body>
 </html>
